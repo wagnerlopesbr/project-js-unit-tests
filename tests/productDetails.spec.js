efc2a1a1b78197e28a724a98475d6a62b6a94082
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-04 15:39:56 -0300

    WL - JS Unit Tests 6/10 v1

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,28 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(productDetails('a', 'b')).toBeDefined();
     // Teste se o retorno da função é um array.
+    expect(typeof productDetails('a', 'b')).toBe('object');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('a', 'b')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('a', 'b')[0]).toBe('object');
+    expect(typeof productDetails('a', 'b')[1]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('a', 'b')[0]).not.toMatchObject(productDetails('a', 'b')[1]);
     // Teste se os dois productIds terminam com 123.
+    function checkIf123() {
+      const test = productDetails('a', 'b');
+      for (const i of test) {
+        if (!i.details.productId.endsWith('123')) {
+          return false;
+        }
+        return true;
+      }
+    }
+    expect(checkIf123()).toBe(true);
   });
 });
