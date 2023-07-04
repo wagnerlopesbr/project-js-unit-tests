f3fed3bdbe09a0a05e12b556401351a18537379d
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-04 12:25:10 -0300

    WL - JS Unit Tests 3/10 v3

--- src/vqv.js
+++ src/vqv.js
@@ -22,9 +22,10 @@ const vqv = (name, age) => {
   if (!name && !age) {
     return undefined;
   }
-  const vqvText = `Oi, meu nome é ${name}!\n
-  Tenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
-  return vqvText;
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
